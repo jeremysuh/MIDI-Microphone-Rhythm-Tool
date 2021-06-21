77d5cc9d12691e8fd334bba1fa5d5fe29c24ed38
Jeung Hwan Suh <jeunghwansuh@gmail.com>
2021-06-21 13:49:34 +0900

    Add midi file upload

--- src/MidiMicrophoneTool.tsx
+++ src/MidiMicrophoneTool.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+//import { Midi, MidiJSON } from "@tonejs/midi";
+import { useEffect, useRef, useState } from "react";
 import "./MidiMicrophoneTool.css";
 
 const MidiPanel = () => {
@@ -11,6 +12,26 @@ const AudioRecorder = () => {
 
 function MidiMicrophoneTool() {
     const [activated, setActivated] = useState<boolean>(false);
+    const [midiUri, setMidiUri] = useState<string | null>("");
+    //const [midiArrayBuffer, setMidiArrayBuffer] = useState<ArrayBuffer | null>(null);
+    //const [midiJSON, setMidiJSON] = useState<MidiJSON | null>(null);
+    const fileReader = useRef<FileReader>(new FileReader());
+
+    useEffect(() => {
+        if (midiUri === "") return;
+        const setMidiObject = async () => {
+            // await Midi.fromUrl(midiUri as string).then((json : MidiJSON) => setMidiJSON(json));
+        };
+        setMidiObject();
+    }, [midiUri]);
+
+    useEffect(() => {
+        fileReader.current.onload = () => {
+            const result: string | ArrayBuffer | null = fileReader.current.result;
+            if (result === null) return;
+            if (typeof result === "string") setMidiUri(result);
+        };
+    }, []);
 
     const onStart = () => {
         setActivated(true);
@@ -22,14 +43,37 @@ function MidiMicrophoneTool() {
         console.log("stop");
     };
 
+    const readMidiUri = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files === null) return;
+        if (event.target.files.length !== 1) return;
+
+        const file = event.target.files[0];
+        if (file === undefined || file === null) return;
+
+        const fileExtension = file.name.split('.').pop();
+        if (fileExtension === undefined || 
+            (fileExtension.toLowerCase() !== 'mid' && fileExtension.toLowerCase() !== 'midi')) { 
+                console.log("file invalid")
+                return;
+            }
+
+        fileReader.current.readAsDataURL(file); //triggers onload function
+        //fileReader.current.readAsArrayBuffer(file);
+        //fileReader.current.readAsText(file);
+    };
+
     return (
         <div className="App">
             <h1>{"MIDI & Microphone Rhythm Practice"}</h1>
+            <input type="file" accept="audio/midi, audio/mid" onChange={(event) => readMidiUri(event)}></input>
             <MidiPanel />
             <AudioRecorder />
             <div>{`Status: ${activated ? "Running" : "Stopped"}`}</div>
             <button onClick={() => onStart()}>Start</button>
             <button onClick={() => onStop()}>Stop</button>
+            {/* <div style={{ textAlign: "center", borderStyle: "solid", margin: "1em", maxWidth: "85%" }}>
+                {midiJSON === null ? "No MIDI file selected" : JSON.stringify(midiJSON)}
+            </div> */}
         </div>
     );
 }
