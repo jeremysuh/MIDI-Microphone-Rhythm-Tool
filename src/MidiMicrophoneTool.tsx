4df035877b0ee63273242d92447ed03b69e7e348
Jeung Hwan Suh <jeunghwansuh@gmail.com>
2021-06-21 16:39:10 +0900

    Add midi play

--- src/MidiMicrophoneTool.tsx
+++ src/MidiMicrophoneTool.tsx
@@ -1,6 +1,8 @@
 //import { Midi, MidiJSON } from "@tonejs/midi";
 import { useEffect, useRef, useState } from "react";
 import "./MidiMicrophoneTool.css";
+import MidiPlayer from "midi-player-js";
+import Soundfont from "soundfont-player";
 
 const MidiPanel = () => {
     return <div>Midi Panel</div>;
@@ -12,16 +14,34 @@ const AudioRecorder = () => {
 
 function MidiMicrophoneTool() {
     const [activated, setActivated] = useState<boolean>(false);
-    const [midiUri, setMidiUri] = useState<string | null>("");
+    const [midiUri, setMidiUri] = useState<string | null>(null);
+    const [soundFontLoaded, setSoundFontLoaded] = useState<boolean>(false);
     //const [midiArrayBuffer, setMidiArrayBuffer] = useState<ArrayBuffer | null>(null);
     //const [midiJSON, setMidiJSON] = useState<MidiJSON | null>(null);
     const fileReader = useRef<FileReader>(new FileReader());
+    const ac = useRef<AudioContext>(new AudioContext());
+
+    const player = useRef<MidiPlayer.Player>(new MidiPlayer.Player());
+    const soundInstrument = useRef<Soundfont.Player | null>(null);
 
     useEffect(() => {
-        if (midiUri === "") return;
+        Soundfont.instrument(ac.current, "acoustic_guitar_nylon").then(function (instrument: Soundfont.Player) {
+            soundInstrument.current = instrument;
+            setSoundFontLoaded(true);
+            player.current = new MidiPlayer.Player(function (event: any) {
+                if (event.name === "Note on" && event.velocity > 0) {
+                    instrument.play(event.noteName, ac.current.currentTime, { gain: event.velocity / 100 });
+                }
+            });
+        });
+    }, []);
+
+    useEffect(() => {
+        if (midiUri === null) return;
         const setMidiObject = async () => {
             // await Midi.fromUrl(midiUri as string).then((json : MidiJSON) => setMidiJSON(json));
         };
+        player.current.loadDataUri(midiUri);
         setMidiObject();
     }, [midiUri]);
 
@@ -34,12 +54,23 @@ function MidiMicrophoneTool() {
     }, []);
 
     const onStart = () => {
+        if (player.current.isPlaying()) return;
         setActivated(true);
+        player.current.play();
         console.log("start");
     };
 
-    const onStop = () => {
+    const onPause = () => {
+        if (player.current.isPlaying() === false) return;
         setActivated(false);
+        player.current.pause();
+        console.log("pause");
+    };
+
+    const onStop = () => {
+        if (player.current.isPlaying() === false) return;
+        setActivated(false);
+        player.current.stop();
         console.log("stop");
     };
 
@@ -50,12 +81,11 @@ function MidiMicrophoneTool() {
         const file = event.target.files[0];
         if (file === undefined || file === null) return;
 
-        const fileExtension = file.name.split('.').pop();
-        if (fileExtension === undefined || 
-            (fileExtension.toLowerCase() !== 'mid' && fileExtension.toLowerCase() !== 'midi')) { 
-                console.log("file invalid")
-                return;
-            }
+        const fileExtension = file.name.split(".").pop();
+        if (fileExtension === undefined || (fileExtension.toLowerCase() !== "mid" && fileExtension.toLowerCase() !== "midi")) {
+            console.log("file invalid");
+            return;
+        }
 
         fileReader.current.readAsDataURL(file); //triggers onload function
         //fileReader.current.readAsArrayBuffer(file);
@@ -65,12 +95,19 @@ function MidiMicrophoneTool() {
     return (
         <div className="App">
             <h1>{"MIDI & Microphone Rhythm Practice"}</h1>
-            <input type="file" accept="audio/midi, audio/mid" onChange={(event) => readMidiUri(event)}></input>
+            <input type="file" accept="audio/midi, audio/mid" onChange={(event) => readMidiUri(event)} disabled={!soundFontLoaded}></input>
             <MidiPanel />
             <AudioRecorder />
             <div>{`Status: ${activated ? "Running" : "Stopped"}`}</div>
-            <button onClick={() => onStart()}>Start</button>
-            <button onClick={() => onStop()}>Stop</button>
+            <button onClick={() => onStart()} disabled={midiUri === null}>
+                Start
+            </button>
+            <button onClick={() => onPause()} disabled={midiUri === null}>
+                Pause
+            </button>
+            <button onClick={() => onStop()} disabled={midiUri === null}>
+                Stop
+            </button>
             {/* <div style={{ textAlign: "center", borderStyle: "solid", margin: "1em", maxWidth: "85%" }}>
                 {midiJSON === null ? "No MIDI file selected" : JSON.stringify(midiJSON)}
             </div> */}
