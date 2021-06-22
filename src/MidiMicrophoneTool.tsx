9b4776ee1bb12a011604397e9db034bdcacc63c0
Jeung Hwan Suh <jeunghwansuh@gmail.com>
2021-06-22 16:36:40 +0900

    Add microphone record and playback

--- src/MidiMicrophoneTool.tsx
+++ src/MidiMicrophoneTool.tsx
@@ -24,6 +24,58 @@ function MidiMicrophoneTool() {
     const player = useRef<MidiPlayer.Player>(new MidiPlayer.Player());
     const soundInstrument = useRef<Soundfont.Player | null>(null);
 
+    const mediaStream = useRef<MediaStream | null>(null);
+    const mediaRecorder = useRef<MediaRecorder | null>(null);
+    const [microphoneAccess, setMicrophoneAccess] = useState<boolean>(false);
+    const [recording, setRecording] = useState<boolean>(false);
+
+    const [audioSrc, setAudioSrc] = useState<string | null>(null);
+    const chunks = useRef<Blob[]>([]);
+
+    const allowMicrophoneAccess = () => {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            console.log("getUserMedia supported.");
+            navigator.mediaDevices
+                .getUserMedia(
+                    // constraints - only audio needed for this app
+                    {
+                        audio: true,
+                    }
+                )
+                // Success callback
+                .then(function (stream: MediaStream) {
+                    mediaStream.current = stream;
+                    const recorder = new MediaRecorder(stream);
+                    recorder.onstart = function (e) {
+                        setRecording(true);
+                    };
+                    recorder.ondataavailable = function (e: BlobEvent) {
+                        //chunks.push(e.data);
+                        console.log("on data available");
+                        console.log(e.data);
+                        chunks.current.push(e.data);
+                    };
+                    recorder.onstop = function (e) {
+                        const blob = new Blob(chunks.current, { type: "audio/ogg; codecs=opus" });
+                        chunks.current = [];
+                        const audioURL = window.URL.createObjectURL(blob);
+                        setAudioSrc(audioURL);
+                        setRecording(false); //<-not on startrecord function; file delay
+                    };
+                    mediaRecorder.current = recorder;
+
+                    setMicrophoneAccess(true);
+                })
+
+                // Error callback
+                .catch(function (err) {
+                    console.log("The following getUserMedia error occurred: " + err);
+                });
+        } else {
+            console.log("getUserMedia not supported on your browser!");
+        }
+    };
+
     useEffect(() => {
         Soundfont.instrument(ac.current, "acoustic_guitar_nylon").then(function (instrument: Soundfont.Player) {
             soundInstrument.current = instrument;
@@ -52,6 +104,20 @@ function MidiMicrophoneTool() {
             if (typeof result === "string") setMidiUri(result);
         };
     }, []);
+
+    const recordStart = () => {
+        if (recording || mediaRecorder.current === null) return;
+        mediaRecorder.current.start();
+        console.log(mediaRecorder.current.state);
+        console.log("recorder started");
+    };
+
+    const recordStop = () => {
+        if (!recording || mediaRecorder.current === null) return;
+        mediaRecorder.current.stop();
+        console.log(mediaRecorder.current.state);
+        console.log("recorder stopped");
+    };
 
     const onStart = () => {
         if (player.current.isPlaying()) return;
@@ -108,6 +174,24 @@ function MidiMicrophoneTool() {
             <button onClick={() => onStop()} disabled={midiUri === null}>
                 Stop
             </button>
+            <div>
+                <button onClick={() => allowMicrophoneAccess()} disabled={microphoneAccess}>
+                    Allow Microphone Access
+                </button>
+                <button disabled={!microphoneAccess} onClick={() => recordStart()}>
+                    Record
+                </button>
+                <button disabled={!microphoneAccess} onClick={() => recordStop()}>
+                    Stop
+                </button>
+            </div>
+            <div>
+                {audioSrc && !recording ? (
+                    <audio controls>
+                        <source src={audioSrc as string} type="audio/ogg" />
+                    </audio>
+                ) : null}
+            </div>
             {/* <div style={{ textAlign: "center", borderStyle: "solid", margin: "1em", maxWidth: "85%" }}>
                 {midiJSON === null ? "No MIDI file selected" : JSON.stringify(midiJSON)}
             </div> */}
