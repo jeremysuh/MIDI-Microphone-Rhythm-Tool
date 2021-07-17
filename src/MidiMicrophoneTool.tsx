9269f8b8b1ab2995628e43f53f38d53d5a71c72c
Jeung Hwan Suh <jeunghwansuh@gmail.com>
2021-07-17 18:15:32 +0900

    Set custom start time

--- src/MidiMicrophoneTool.tsx
+++ src/MidiMicrophoneTool.tsx
@@ -1,20 +1,25 @@
 //import { Midi, MidiJSON } from "@tonejs/midi";
 import { useEffect, useRef, useState } from "react";
 import "./MidiMicrophoneTool.css";
-import MidiPlayer from "midi-player-js";
 import Soundfont from "soundfont-player";
+import { Midi, MidiJSON } from "@tonejs/midi";
+import { Range } from "rc-slider";
+import 'rc-slider/assets/index.css'
+//import MidiPlayerJS from "midi-player-js";
+import * as MidiPlayerJS from './midi-player-js/player';
 
-// const MidiPanel = () => {
-//     return <div>Midi Panel</div>;
-// };
+type MidiInformation = {
+    totalLength: number;
+};
 
-// const AudioRecorder = () => {
-//     return <div>Audio Recorder</div>;
-// };
+type Config = {
+    startTime: number;
+    endTime: number;
+};
 
 function MidiMicrophoneTool() {
     //const [midiArrayBuffer, setMidiArrayBuffer] = useState<ArrayBuffer | null>(null);
-    //const [midiJSON, setMidiJSON] = useState<MidiJSON | null>(null);
+    const [midiJSON, setMidiJSON] = useState<MidiJSON | null>(null);
 
     const [midiUri, setMidiUri] = useState<string | null>(null);
     const [soundFontLoaded, setSoundFontLoaded] = useState<boolean>(false);
@@ -22,7 +27,7 @@ function MidiMicrophoneTool() {
     const fileReader = useRef<FileReader>(new FileReader());
     const ac = useRef<AudioContext>(new AudioContext());
 
-    const player = useRef<MidiPlayer.Player>(new MidiPlayer.Player());
+    const player = useRef<MidiPlayerJS.Player>(new MidiPlayerJS.Player()); // useRef<MidiPlayer.Player>(new MidiPlayer.Player());
     const soundInstrument = useRef<Soundfont.Player | null>(null);
 
     const mediaStream = useRef<MediaStream | null>(null);
@@ -35,6 +40,16 @@ function MidiMicrophoneTool() {
     const chunks = useRef<Blob[]>([]);
 
     const audioRef = useRef<HTMLAudioElement>(null); //change legacy ref later
+
+
+
+    const [midiInformation, setMidiInformation] = useState<MidiInformation>({
+        totalLength: 0,
+    });
+    const [config, setConfig] = useState<Config>({
+        startTime: 0,
+        endTime: 0,
+    });
 
     const allowMicrophoneAccess = () => {
         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -83,18 +98,41 @@ function MidiMicrophoneTool() {
         Soundfont.instrument(ac.current, "acoustic_guitar_nylon").then(function (instrument: Soundfont.Player) {
             soundInstrument.current = instrument;
             setSoundFontLoaded(true);
-            player.current = new MidiPlayer.Player(function (event: any) {
+            player.current = new MidiPlayerJS.Player(function (event: any) {
                 if (event.name === "Note on" && event.velocity > 0) {
                     instrument.play(event.noteName, ac.current.currentTime, { gain: event.velocity / 100 });
                 }
             });
+            player.current.on("fileLoaded", function () {
+                setMidiInformation({totalLength: player.current.getSongTime()});
+                setConfig(({
+                    startTime: 0, 
+                    endTime: player.current.getSongTime()
+                }))
+            });
+            player.current.on("endOfFile", function() {
+                //identical to stopprecording function, handles when music is played until end, and not through user button click
+                if (mediaRecorder.current === null) return;
+                if (mediaRecorder.current.state === "inactive") return //for preview listening, media recorder would be inactive, 
+                // so continuing on would result in a crash
+                mediaRecorder.current.stop();
+                player.current.stop(); //not sure if needed
+                console.log("stopped")
+            })
         });
     }, []);
 
     useEffect(() => {
+        console.log(midiJSON);
+    }, [midiJSON]);
+    
+
+    useEffect(() => {
         if (midiUri === null) return;
         const setMidiObject = async () => {
-            // await Midi.fromUrl(midiUri as string).then((json : MidiJSON) => setMidiJSON(json));
+            await Midi.fromUrl(midiUri as string).then((midi: Midi) => {
+                setMidiJSON(midi.toJSON());
+            });
         };
         player.current.loadDataUri(midiUri);
         setMidiObject();
@@ -111,6 +149,15 @@ function MidiMicrophoneTool() {
         };
     }, []);
 
+    useEffect(() => {
+        if (player.current) {
+          player.current.skipToSeconds(config.startTime); //note i edited the index.d.ts file; not sure if this will cause error
+        }
+    }, [config]);
+
+    useEffect(() => {
+    }, [midiInformation])
+
     const readMidiUri = async (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files === null) return;
         if (event.target.files.length !== 1) return;
@@ -119,7 +166,10 @@ function MidiMicrophoneTool() {
         if (file === undefined || file === null) return;
 
         const fileExtension = file.name.split(".").pop();
-        if (fileExtension === undefined || (fileExtension.toLowerCase() !== "mid" && fileExtension.toLowerCase() !== "midi")) {
+        if (
+            fileExtension === undefined ||
+            (fileExtension.toLowerCase() !== "mid" && fileExtension.toLowerCase() !== "midi")
+        ) {
             console.log("file invalid");
             return;
         }
@@ -131,7 +181,7 @@ function MidiMicrophoneTool() {
         if (isRecording || mediaRecorder.current === null) return;
         mediaRecorder.current.start();
         console.log(mediaRecorder.current.state);
-        player.current.play();
+        player.current.skipToSeconds(config.startTime).play();
     };
 
     const stopRecording = () => {
@@ -146,13 +196,14 @@ function MidiMicrophoneTool() {
         if (player.current.isPlaying()) return;
         audioRef.current.play();
         audioRef.current.addEventListener("ended", stopPreview);
-        player.current.play();
+        player.current.skipToSeconds(config.startTime + audioRef.current.currentTime).play();
         setIsPreviewPlaying(true);
     };
 
     const pausePreview = () => {
         if (audioRef.current === null) return;
         if (!player.current.isPlaying()) return;
+        audioRef.current.removeEventListener("ended", stopPreview);
         audioRef.current.pause();
         player.current.pause();
         setIsPreviewPlaying(false);
@@ -181,6 +232,25 @@ function MidiMicrophoneTool() {
                 disabled={!soundFontLoaded || isRecording || isPreviewPlaying}
             ></input>
             <br />
+            <div>{`MIDI Length (seconds): ${midiLoaded ? midiInformation.totalLength : `-`}`}</div>
+            <div style={{display:"flex", justifyContent: "center", flexDirection: "column", alignItems: "center", minWidth: "50%"}}>
+                <Range
+                    defaultValue={[0, 0]}
+                    allowCross={false}
+                    disabled={isRecording}
+                    min={0}
+                    max={midiInformation.totalLength}
+                    value={[config.startTime, midiInformation.totalLength]}
+                    onChange={(value: number[]) => {
+                        setConfig({
+                            startTime: value[0],
+                            endTime: value[1],
+                        });
+                    }}
+                />
+                <div>Start Time: {config.startTime}</div>
+                <div>End Time: {config.endTime}</div>
+            </div>
             <br />
             <button onClick={() => allowMicrophoneAccess()} disabled={hasMicrophoneAccess}>
                 Allow Microphone Access
@@ -198,6 +268,7 @@ function MidiMicrophoneTool() {
             <button onClick={() => playPreview()} disabled={disablePreview}>
                 Play Preview
             </button>
+            {/* {pause button currently ineffective when when pause & resuming; player starts back at config.startime instead} */}
             <button onClick={() => pausePreview()} disabled={disablePreview}>
                 Pause Preview
             </button>
@@ -207,6 +278,9 @@ function MidiMicrophoneTool() {
             <br />
             <br />
             <div>
+                {/* might have to change this approach as buffering issues are causing delay/problems in Chrome */}
+                {/* or implement the player something like this */}
+                {/* {https://letsbuildui.dev/articles/building-an-audio-player-with-react-hooks} */}
                 {audioSrc && !isRecording ? (
                     <audio controls ref={audioRef}>
                         <source src={audioSrc as string} type="audio/ogg" />
